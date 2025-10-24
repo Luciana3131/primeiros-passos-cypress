5935eeec4e673872e6af61023313844463af2100
Luciana Luize <lucianaluize2016@gmail.com>
2025-10-24 20:32:06 -0300

    added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,6 +1,16 @@
 describe('Orange HRM test', () => {// nome do site ou projeto que estou automatizando
   //nome da pagina ou automaçao que estou fazendo
 
+
+
+  const SelectorsList = {
+     usernameField: '[name="username"]' ,
+     passwordField: '[name="password"]' ,
+     loginButton: '.oxd-button' ,
+     sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text' ,
+     wrongCredentialAtert: '.oxd-alert'
+
+  }
   it('Login com sucesso', () => {
   //it.skip - colocar .skip para nao executar esse bloco de teste
 
@@ -8,17 +18,17 @@ describe('Orange HRM test', () => {// nome do site ou projeto que estou automati
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
    
     //preenche o campo do usuario
-    cy.get('[name="username"]').type('Admin')
+    cy.get(SelectorsList.usernameField).type('Admin')
 
     // preenche o campo da senha
-    cy.get('[name="password"]').type('admin123')
+    cy.get(SelectorsList.passwordField).type('admin123')
     
     //aperta o botao entrar
-    cy.get('.oxd-button').click()
+    cy.get(SelectorsList.loginButton).click()
 
     // verifica se estou na pagina correta
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+    cy.get(SelectorsList.sectionTitleTopBar).contains('Dashboard')
   }) 
 
 
@@ -28,15 +38,15 @@ describe('Orange HRM test', () => {// nome do site ou projeto que estou automati
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
    
     //preenchendo o campo do usuario com nome invalido
-    cy.get('[name="username"]').type('test')
+    cy.get(SelectorsList.usernameField).type('test')
 
     // preenchendo o campo da senha com senha errada
-    cy.get('[name="password"]').type('test')
+    cy.get(SelectorsList.passwordField).type('test')
     
     //apertar o botao entrar e retornar menssagem de erro
-    cy.get('.oxd-button').click()
+    cy.get(SelectorsList.loginButton).click()
     
     //verifica a menssagem de erro
-    cy.get('.oxd-alert')
+    cy.get(SelectorsList.wrongCredentialAtert)
   })
 })
